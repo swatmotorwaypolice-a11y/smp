1ff1cf70aeff9d6a1a1962ca1bd6d7a23c79204c
swatmotorwaypolice-a11y <swatmotorwaypolice@gmail.com>
2026-05-05 11:14:23 +0500

    Update licenseData.ts

--- app/driving-license/licenseData.ts
+++ app/driving-license/licenseData.ts
@@ -832,20 +832,6 @@ export const mockLicenseData: { [key: string]: LicenseDetails } = {
     bloodGroup: 'N/A',
     imageUrl: '/TILAWATKHAN.jpg'
   },
-  
-  '2120362671519': {
-    cnic: '21203-6267151-9',
-    name: 'AJMIR SHARIF',
-    fatherName: 'IZAT KHAN',
-    licenseNumber: '00000094573',
-    licenseType: 'HTV ONLY',
-    issueDate: '22-02-2025',
-    expiryDate: '16-01-2030',
-    address: "null",
-    bloodGroup: 'N/A',
-    imageUrl: '/AJMIR.jpg'
-  },
-  
   '2120377132189': {
     cnic: '21203-7713218-9',
     name: 'ABDULLAH',
@@ -992,6 +978,18 @@ export const mockLicenseData: { [key: string]: LicenseDetails } = {
     address: "null",
     bloodGroup: 'N/A',
     imageUrl: '/MUSADIQKHAN.jpg'
+  },
+   '5420340693543': {
+    cnic: '54203-4069354-3',
+    name: 'ABDUL REHMAN',
+    fatherName: 'ABDUL QAYYUM',
+    licenseNumber: '00000080641',
+    licenseType: 'HTV ONLY',
+    issueDate: '27-03-2025',
+    expiryDate: '11-02-2030',
+    address: "null",
+    bloodGroup: 'N/A',
+    imageUrl: '/ABDULREHMAN.jpg'
   },
 };
 
